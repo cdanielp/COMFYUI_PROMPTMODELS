68fd2902a09ce8d1c91f7002271fe534b2e5bbb4
cdanielp <38308141+cdanielp@users.noreply.github.com>
2025-12-04 11:03:07 -0600

    Update __init__.py

--- get_last_frame/__init__.py
+++ get_last_frame/__init__.py
@@ -1,3 +1,4 @@
 from .get_last_frame import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+print("[ComfyDeploy] ✅ get_last_frame importado correctamente")
